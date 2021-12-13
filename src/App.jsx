a009d1ef12a83f72b12514819d8d16a065fa0ec6
Alexey Ilin <ilin.al.vick@gmail.com>
2021-12-13 13:44:57 +0300

    markup: сверстал игровой экран

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+//import doge from "../public/img/doge.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <img className="doge" src="img/doge.svg" alt="doge" />
+      <img className="stars" src="img/stars.svg" alt="stars" />
+    </div>
+  );
+}
+
+export default App;
