176983c2d828232a936756d8ec19a05b7ed97bdf
Alexey Ilin <ilin.al.vick@gmail.com>
2021-12-14 23:13:12 +0300

    feat: собака запрыгивает на платформу

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 function App() {
   const [inJump, setInJump] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [onPlatform, setOnPlatform] = useState({});
 
   const fromJump = () => {
     setInJump(false);
@@ -12,15 +14,57 @@ function App() {
     setTimeout(fromJump, 700);
   };
 
+  let dogeElem = useRef();
+  let dogePosition;
+  let platformElem = useRef();
+  let platformPosition;
+
+  const checkOnPlatform = () => {
+    dogePosition = dogeElem.current.getBoundingClientRect();
+    platformPosition = platformElem.current.getBoundingClientRect();
+    if (
+      platformPosition.left <= dogePosition.right &&
+      dogePosition.right <= platformPosition.right &&
+      dogePosition.bottom >= platformPosition.top &&
+      dogePosition.bottom <= platformPosition.top + 10
+    ) {
+      let platformY = getComputedStyle(platformElem.current).bottom;
+      let px = +platformY.match(/\d+/)[0] + 40;
+
+      setOnPlatform({
+        bottom: px + "px",
+        transition: 0.1 + "s",
+      });
+    }
+  };
+
+  useEffect(() => {
+    setIsLoaded(true);
+    dogeElem.current = document.querySelector(".doge");
+    platformElem.current = document.querySelector(".platform");
+    console.log(getComputedStyle(dogeElem.current).bottom);
+  }, []);
+
+  if (isLoaded === true) {
+    setInterval(checkOnPlatform, 1);
+  }
+
   return (
     <div onClick={onJump} className="App">
       <img
+        ref={dogeElem}
+        style={onPlatform}
         className={"doge" + (inJump ? " jump" : "")}
         src="img/doge.svg"
         alt="doge"
       />
       <img className="stars flying" src="img/stars.svg" alt="stars" />
-      <img className="platform flying" src="img/platform.svg" alt="platform" />
+      <img
+        ref={platformElem}
+        className="platform flying"
+        src="img/platform.svg"
+        alt="platform"
+      />
     </div>
   );
 }
